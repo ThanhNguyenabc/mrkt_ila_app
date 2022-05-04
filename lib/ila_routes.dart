8dd35751218dcad488071c8f0e4e4b477825109c
thanhnguyenABC <thanhnguyen95abc@gmail.com>
2022-05-04 16:17:58 +0700

    add ila routes

--- lib/ila_routes.dart
+++ lib/ila_routes.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mrkt_app/features/auth/auth_route.dart';
-import 'package:mrkt_app/features/home/home_page.dart';
+import 'package:mrkt_app/features/home/main_page.dart';
 import 'package:mrkt_app/features/landing_page/landing_page.dart';
 import 'package:mrkt_app/features/profile/profile_route.dart';
+import 'package:mrkt_app/features/register/register_route.dart';
 
 class IlaRoute {
   static Map<String, Widget Function(BuildContext)> allRoutes() {
     return {
       LandingPage.route: (_) => const LandingPage(),
-      HomePage.route: (_) => const HomePage(),
-      ...getAuthRoutes(),
-      ...getProfileRoutes()
+      MainPage.route: (_) => const MainPage(),
+      ...registerRoutes(),
+      ...authRoutes(),
+      ...profileRoutes()
     };
   }
 }
