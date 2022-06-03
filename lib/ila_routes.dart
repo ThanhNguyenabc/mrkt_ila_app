0d6f42b8cafc9af32045fe5c661ace166d39925d
thanhnguyenABC <thanhnguyen95abc@gmail.com>
2022-06-03 13:41:55 +0700

    add web_in_app route to app route

--- lib/ila_routes.dart
+++ lib/ila_routes.dart
@@ -5,13 +5,18 @@ import 'package:mrkt_app/screens/home/main_page.dart';
 import 'package:mrkt_app/screens/landing/landing_page.dart';
 import 'package:mrkt_app/screens/profile/profile_route.dart';
 import 'package:mrkt_app/screens/register/register_route.dart';
+import 'package:mrkt_app/widget/web_in_app.dart';
 
 class IlaRoute {
-  static Map<String, Widget Function(BuildContext)> allRoutes() {
+  static Map<String, WidgetBuilder> allRoutes() {
     return {
       LandingPage.route: (_) => const LandingPage(),
       MainPage.route: (_) => const MainPage(),
       CEFRLevelPage.route: (_) => const CEFRLevelPage(),
+      WebInApp.route: (context) => WebInApp(
+            params:
+                ModalRoute.of(context)?.settings.arguments as WebInAppParams?,
+          ),
       ...registerRoutes(),
       ...authRoutes(),
       ...profileRoutes()
